1a8a412bf77be2dffc4da1c776d7d6a6bf2d7349
mox-labs <mox-labs@users.noreply.github.com>
2026-02-04 10:22:57 +0000

    Deploy preview for PR 4 🛫

--- pr-preview/pr-4/searcher-c2a407aa.js
+++ pr-preview/pr-4/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-670c697b.js',
+                path_to_root + 'searchindex-80c39d86.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
