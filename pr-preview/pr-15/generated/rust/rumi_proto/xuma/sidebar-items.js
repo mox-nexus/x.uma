2770e6ac62eea124ad1ddce8cb0caddd5718e522
mox-nexus <mox-nexus@users.noreply.github.com>
2026-02-14 04:10:21 +0000

    Deploy preview for PR 15 🛫

---
+++ pr-preview/pr-15/generated/rust/rumi_proto/xuma/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["claude","core","http","test"]};
---
+++ pr-preview/pr-15/generated/rust/trait.impl/core/convert/trait.TryFrom.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rumi_proto",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.93.1/std/primitive.i32.html\">i32</a>&gt; for <a class=\"enum\" href=\"rumi_proto/xds/core/v3/resource_locator/enum.Scheme.html\" title=\"enum rumi_proto::xds::core::v3::resource_locator::Scheme\">Scheme</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[438]}
