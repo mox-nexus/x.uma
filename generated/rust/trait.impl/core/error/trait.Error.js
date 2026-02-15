0f288fc0fb08a8442c53c65b1ae041d6bcf3d84c
mox-nexus <mox-nexus@users.noreply.github.com>
2026-02-15 12:05:36 +0000

    deploy: 0ba49eb9c3b0abcadb28da8c62f1d444541b0d2c

--- generated/rust/trait.impl/core/error/trait.Error.js
+++ generated/rust/trait.impl/core/error/trait.Error.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["rumi",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"rumi/enum.MatcherError.html\" title=\"enum rumi::MatcherError\">MatcherError</a>"]]],["rumi_claude",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"rumi_claude/struct.CompileError.html\" title=\"struct rumi_claude::CompileError\">CompileError</a>"]]]]);
+    var implementors = Object.fromEntries([["rumi",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"rumi/enum.MatcherError.html\" title=\"enum rumi::MatcherError\">MatcherError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[263,291]}
+//{"start":57,"fragment_lengths":[263]}
--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6b1472f2.js',
+                path_to_root + 'searchindex-86c44684.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
