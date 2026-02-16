0d9263a74e9660a8033e9b7e21923b2e4c77b62c
mox-nexus <mox-nexus@users.noreply.github.com>
2026-02-16 11:24:17 +0000

    Deploy preview for PR 17 🛫

---
+++ pr-preview/pr-17/generated/rust/crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["bumi_crusty","puma_crusty","rumi","rumi_claude","rumi_http","rumi_test"];
+//{"start":21,"fragment_lengths":[13,14,7,14,12,12]}
---
+++ pr-preview/pr-17/generated/rust/trait.impl/core/cmp/trait.PartialEq.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rumi",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"rumi/enum.MatcherError.html\" title=\"enum rumi::MatcherError\">MatcherError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"rumi/enum.MatchingData.html\" title=\"enum rumi::MatchingData\">MatchingData</a>"]]],["rumi_claude",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"rumi_claude/enum.HookEvent.html\" title=\"enum rumi_claude::HookEvent\">HookEvent</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[532,284]}
