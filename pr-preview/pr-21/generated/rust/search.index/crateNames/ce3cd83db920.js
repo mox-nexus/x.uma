e94afa42281a09775ff0027d12c3cd478dcb281b
yzavyas <yzavyas@users.noreply.github.com>
2026-02-26 19:57:57 +0000

    Deploy preview for PR 21 🛫

---
+++ pr-preview/pr-21/generated/rust/search.index/crateNames/ce3cd83db920.js
@@ -0,0 +1,1 @@
+rd_("drumiirumi_httpirumi_test")
---
+++ pr-preview/pr-21/generated/rust/trait.impl/serde_core/ser/trait.Serialize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rumi",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"rumi/claude/enum.HookEvent.html\" title=\"enum rumi::claude::HookEvent\">HookEvent</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"rumi/enum.StringMatchSpec.html\" title=\"enum rumi::StringMatchSpec\">StringMatchSpec</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"rumi/claude/struct.ArgumentMatch.html\" title=\"struct rumi::claude::ArgumentMatch\">ArgumentMatch</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"rumi/claude/struct.HookMatch.html\" title=\"struct rumi::claude::HookMatch\">HookMatch</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1161]}
