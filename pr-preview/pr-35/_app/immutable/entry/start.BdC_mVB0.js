6e89cc4014d4f7e65d18c007d7a4040756c2b5a6
yzavyas <yzavyas@users.noreply.github.com>
2026-08-18 13:49:54 +0000

    Deploy preview for PR 35 🛫

---
+++ pr-preview/pr-35/_app/immutable/entry/start.BdC_mVB0.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DEjjbYIs.js";import{z as t}from"../chunks/IZaYnyk_.js";export{t as load_css,r as start};
---
+++ pr-preview/pr-35/api/rust/trait.impl/core/cmp/trait.PartialEq.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["rumi",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"rumi/claude/enum.HookEvent.html\" title=\"enum rumi::claude::HookEvent\">HookEvent</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"rumi/enum.MatcherError.html\" title=\"enum rumi::MatcherError\">MatcherError</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"rumi/enum.MatchingData.html\" title=\"enum rumi::MatchingData\">MatchingData</a>",0]]],["rumi",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"rumi/enum.Domain.html\" title=\"enum rumi::Domain\">Domain</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"rumi/hook/struct.HookPayload.html\" title=\"struct rumi::hook::HookPayload\">HookPayload</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[805,533]}
