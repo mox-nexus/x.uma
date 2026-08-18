2770ad71806741349d50c1835fe74fee2187188d
yzavyas <yzavyas@users.noreply.github.com>
2026-08-18 17:57:47 +0000

    Deploy preview for PR 48 🛫

---
+++ pr-preview/pr-48/_app/immutable/entry/start.BLnnp3g8.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/D9WDosN1.js";import{z as t}from"../chunks/B8m74XfO.js";export{t as load_css,r as start};
---
+++ pr-preview/pr-48/api/rust/trait.impl/rumi/registry/trait.IntoAction.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["rumi",[["impl IntoAction&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.95.0/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt; for <a class=\"struct\" href=\"rumi/struct.NamedActionFactory.html\" title=\"struct rumi::NamedActionFactory\">NamedActionFactory</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[317]}
