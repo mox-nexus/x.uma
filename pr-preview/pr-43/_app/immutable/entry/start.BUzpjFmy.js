5edd5867d4447ee60176da750ee77b787eedf087
yzavyas <yzavyas@users.noreply.github.com>
2026-08-18 15:20:52 +0000

    Deploy preview for PR 43 🛫

---
+++ pr-preview/pr-43/_app/immutable/entry/start.BUzpjFmy.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BbjtU4_c.js";import{z as t}from"../chunks/GjELBTi3.js";export{t as load_css,r as start};
---
+++ pr-preview/pr-43/api/rust/trait.impl/rumi/registry/trait.IntoInputMatcher.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["rumi",[]],["rumi_http",[]],["rumi_kv",[]],["rumi_test",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[11,17,15,17]}
